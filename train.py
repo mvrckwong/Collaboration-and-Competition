d6b3e555fb855cf9a36c5ba104adde2ff822636c
Maverick Wong <mav.wong31@gmail.com>
2019-07-31 13:44:42 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,148 +1,100 @@
 from unityagents import UnityEnvironment
-import matplotlib.pyplot as plt
 import numpy as np
-import torch
 
 from agent import Agent
 from collections import deque
 
+import torch
 
-### Interpret the Environment / De-Code the Environment
-def check_env(env, summary=True):
-    # Default Brain
-    brain_name = env.brain_names[0]
-    brain = env.brains[brain_name]
+# Environment !!!
+env = UnityEnvironment(file_name="Tennis_Windows_x86_64/Tennis.exe")
 
-    # Reset the Environment
-    env_info = env.reset(train_mode=True)[brain_name]
+# get the default brain
+brain_name = env.brain_names[0]
+brain = env.brains[brain_name]
 
-    # Number of Agents Available
-    n_agents = len(env_info.agents)
+# reset the environment
+env_info = env.reset(train_mode=True)[brain_name]
 
-    # Size of Each Action
-    action_size = brain.vector_action_space_size
+# number of agents
+num_agents = len(env_info.agents)
 
-    # Examine the State Space
-    states = env_info.vector_observations
-    state_size = states.shape[1]
+# size of each action
+action_size = brain.vector_action_space_size
 
-    if summary==True:
-        print("---------------------------------------------------------------------------")
-        print("Number of agents: {}".format(n_agents))
-        print("Size of each action: {}".format(action_size))
-        print("There are {} agents. Each observes a state with length {}.".format(states.shape[0], state_size))
-        print("---------------------------------------------------------------------------")
-    return state_size, action_size, n_agents
+# examine the state space
+states = env_info.vector_observations
+state_size = states.shape[1]
 
-### Initialize the Agent
-class initialize_MADDPG:
-    def __init__(self,
-                 env=None,
-                 name="MADDPG",
-                 n_episodes=50,
-                 n_timesteps=1000,
-                 n_steps=2):
 
-        self.env = env
-        self.name = name
+agent = Agent(state_size, action_size, random_seed=5)
 
-        self.state_size, self.action_size, self.n_agents = check_env(self.env, True)
-        self.agent = Agent(self.state_size, self.action_size, random_seed)
+def checkpoint(i_episode, scores_window, scores):
+    print("\rEpisode {}\t\tAvg Score: {:.2f}\t\tMax Score: {:2f}".format(i_episode, np.mean(scores_window), np.max(scores)), end="")
+    if i_episode % 100 == 0:
+        print("\rEpisode {}\t\tAvg Score: {:2f}".format(i_episode, np.mean(scores_window), np.mean(scores)))
+    if i_episode % 200 == 0:
+        torch.save(agent.actor_local.state_dict(), 'checkpoint_{}_actor.pth'.format(i_episode))
+        torch.save(agent.critic_local.state_dict(), 'checkpoint_{}_critic.pth'.format(i_episode))
 
-        self.n_episodes = n_episodes
-        self.n_timesteps = n_timesteps
-        self.n_steps = n_steps
+def plot_process(scores):
+    fig = plt.figure(figsize=(16,5))
+    ax = fig.add_subplot(111)
+    plt.plot(np.arange(1, len(scores)+1), scores)
 
-        self.display_freq = 100
-        self.save_at_checkpoint = True
+    plt.grid(which="major", alpha=0.30)
+    plt.title('MADDPG')
+    plt.ylabel('Avg Score across all Agents')
+    plt.xlabel('Number of Episode')
+    plt.savefig('Scores.png')
+    plt.legend(loc=0)
+    plt.show()
 
-    def train(self, exp_number=0):
-        scores = []
-        scores_window = deque(maxlen=self.display_freq)
 
-        for i_episode in range(1, self.n_episodes+1):
-            env_info = self.env.reset(train_mode=True)[brain_name]       # Reset the environment
-            states = env_info.vector_observations                   # Get the current state
-            self.agent.reset()
-            scores = np.zeros(num_agents)
+# Train MADDPG
+def train(n_episodes=40000,
+          n_timesteps=500):
+    scores_window = deque(maxlen=100)
+    scores_agents = []
 
-            #while True:                                 # Consider the agent will solve the environment
-            for t in range(1, self.n_timesteps):
-                actions = self.agent.act(states)
-                env_info = self.env.step(actions)[brain_name]
-                next_states = env_info.vector_observations
-                rewards = env_info.rewards
-                dones = env_info.local_done
+    for i_episode in range(1, n_episodes+1):
+        env_info = env.reset(train_mode=True)[brain_name] # reset environment
+        states = env_info.vector_observations             # get the current state
+        agent.reset()
+        scores = np.zeros(num_agents)
 
-                for state, action, reward, next_state, done in zip(states, actions, rewards, next_states, done):
-                    self.agent.step(state, action, reward, next_state, done)
+        #while True:
+        for t in range(1, n_timesteps+1):
+            actions = agent.act(states)
+            env_info = env.step(actions)[brain_name]     # send the action to the environment
+            next_states = env_info.vector_observations   # get the next state
+            rewards = env_info.rewards                   # get the reward
+            dones = env_info.local_done                  # see if episode has finished
+            for state, action, reward, next_state, done in zip(states, actions, rewards, next_states, dones):
+                agent.step(state, action, reward, next_state, done)
 
-            scores_window.append(np.max(scores))
-            scores_agents.append(np.max(scores))
-            # Checkpoint
-            self.checkpoint(i_episode, scores_window,
-                            exp_number,
-                            self.display_freq,
-                            self.save_at_checkpoint)
+            states = next_states
+            scores += rewards
 
-            if np.mean(scores_window) >= 0.5 and i_episode >= 100:
-                torch.save(agent.actor_local.state_dict(), 'checkpoint_actor.pth')
-                torch.save(agent.critic_local.state_dict(), 'checkpoint_critic.pth')
-                print('\rEnvironment solved in {} episodes (average score {:.2f}).'.format(i_episode-100,np.mean(scores_deque)))
+            if np.any(dones):
                 break
 
-        return scores_agent
+        # from the two scores, we append the better score
+        scores_window.append(np.max(scores))
+        scores_agents.append(np.max(scores))
 
-    def checkpoint(self, i_episode,
-                   scores_window,
-                   exp_number,
-                   display_freq=100,
-                   save_at_checkpoint=False):
+        checkpoint(i_episode, scores_window, scores)
 
-        print("\rEpisode {}\t\tAvg Score: {:.2f}".format(i_episode, np.mean(scores_window), end=""))
-        if i_episode % display_freq == 0:
-            print("\rEpisode {}\t\tAvg Score: {:2f}".format(i_episode, np.mean(scores_window)))
-
-        # Save Model for every 100 episodes
-        if i_episode == 100:
-            #torch.save(policy.state_dict(), model_path + "{2}_{1}_Chkpnt{0}.pth".format(i_episode, exp_number, model_name))
-
-    def test(self, play_episodes):
-        self.agent.actor_local.load_state_dict(torch.load("checkpoint_actor.pth"))
-        self.agent.critic_local.load_state_dict(torch.load("checkpoint_critic.pth"))
-
-        for i in range(1, play_episodes+1):                             # play game for number of episodes
-            env_info = env.reset(train_mode=False)[brain_name]          # reset the environment
-            states = env_info.vector_observations                       # get the current state (for each agent)
-            scores = np.zeros(num_agents)                               # initialize the score (for each agent)
-
-            while True:
-                actions = agent.act(states)                             # select an action (for each agent)
-                actions = np.clip(actions, -1, 1)                       # all actions between -1 and 1
-                env_info = env.step(actions)[brain_name]                # send all actions to the environment
-                next_states = env_info.vector_observations              # get next state (for each agent)
-                rewards = env_info.rewards                              # get reward (for each agent)
-                dones = env_info.local_done                             # see if episode finished
-                scores += env_info.rewards                              # update the score (for each agent)
-                states = next_states                                    # roll over states to next time step
-                if np.any(dones):                                       # exit loop if episode finished
-                    break
-            print('Score (max over agents) from episode {}: {}'.format(i, np.max(scores)))
-
-    def plot_process(self, scores_agent):
-        fig = plt.figure(figsize=(16,5))
-        ax = fig.add_subplot(111)
-        plt.plot(np.arange(1, len(scores_agent)+1), scores)
-
-        plt.grid(which="major", alpha=0.30)
-        plt.title('MADDPG')
-        plt.ylabel('Avg Score across all Agents')
-        plt.xlabel('Number of Episode')
-        plt.savefig('Scores.png')
-        plt.legend(loc=0)
-        plt.show()
-
+        #environment solved for avg score of .5 over past 100 episodes
+        if np.mean(scores_window) >= 0.5:
+            print('\rEnvironment solved in {} episodes (average score {:.2f}).'.format(i_episode, np.mean(scores_window)))
+            torch.save(agent.actor_local.state_dict(), 'final_checkpoint_actor.pth')
+            torch.save(agent.critic_local.state_dict(), 'final_checkpoint_critic.pth')
+            break
+    return scores_agents
 
 
 
 if __name__ == '__main__':
+    scores = train(n_episodes=25000, n_timesteps=700)
+    plot_process(scores)
